753fdefc76b0b01d414f5e8fd91a53eb105d99dc
kskarma <132248434+kskarma@users.noreply.github.com>
2023-05-01 15:34:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import streamlit as st
 # Replace "YOUR_API_KEY" with your actual OpenAI API key
 openai.api_key = "取得したAPIキー"
 
-st.title(st.secrets["DB_KEY"])
+st.title("GPT: ")
+
+text = st.secrets["DB_KEY"]
+st.write = text
 
 # Get the user's message
 message = st.text_input("Enter your message:")
