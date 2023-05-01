a31465b7cf10378f6d17d552243ecaece90c577e
kskarma <132248434+kskarma@users.noreply.github.com>
2023-05-01 14:45:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 def main():
-    st.write("Hello World")
+    # テキストボックスで名前を入力
+    name = st.text_input("名前を入力してください")
+    #名前が入力されていればこんにちはと表示
+    if name:
+        st.write(f"こんにちは、{name}さん！")
 
 if __name__ == "__main__":
     main()
